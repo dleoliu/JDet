546b4ff036db3dc9379f55d85d86044340ab5ee7
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2022-07-28 16:23:06 +0900

    Update configs/rs22/gliding/gliding_config.py

--- configs/rs22/gliding/gliding_config.py
+++ configs/rs22/gliding/gliding_config.py
@@ -1,7 +1,7 @@
 _base_ = 'gliding_r50_fpn_1x_dota_with_flip_rotate_balance_cate.py'
 
 dataset = dict(
-    _cover_ = True,
+    _cover_=True,
     val=dict(
         type="FAIRDataset",
         dataset_dir='/scratch/acd13834xw/FAIR1M2/processed_1024/trainval_1024_200_1.0',
@@ -12,13 +12,15 @@ dataset = dict(
                 max_size=1024
             ),
             dict(
-                type = "Pad",
-                size_divisor=32),
+                type="Pad",
+                size_divisor=32
+            ),
             dict(
-                type = "Normalize",
-                mean =  [123.675, 116.28, 103.53],
-                std = [58.395, 57.12, 57.375],
-                to_bgr=False,)
+                type="Normalize",
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_bgr=False
+            )
         ],
         batch_size=2,
         num_workers=18,
@@ -35,19 +37,22 @@ dataset = dict(
             ),
             dict(
                 type='RotatedRandomFlip',
-                prob=0.5),
-            dict(
-                type="RandomRotateAug",
-                random_rotate_on=True,
+                prob=0.5
             ),
             dict(
-                type = "Pad",
-                size_divisor=32),
+                type="RandomRotateAug",
+                random_rotate_on=True
+            ),
             dict(
-                type = "Normalize",
-                mean =  [123.675, 116.28, 103.53],
-                std = [58.395, 57.12, 57.375],
-                to_bgr=True,)
+                type="Pad",
+                size_divisor=32
+            ),
+            dict(
+                type="Normalize",
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_bgr=True
+            )
         ],
         batch_size=2, 
         num_workers=18,
@@ -60,21 +65,24 @@ dataset = dict(
         dataset_type="FAIR",
         images_dir= "/scratch/acd13834xw/FAIR1M2/processed_1024/test_1024_200_1.0/images/",
         transforms= [
-        dict(
-            type= "RotatedResize",
-            min_size= 1024,
-            max_size= 1024),
-        dict(
-                type = "Pad",
-                size_divisor=32),
-        dict(
-            type= "Normalize",
-            mean=  [123.675, 116.28, 103.53],
-            std= [58.395, 57.12, 57.375],
-            to_bgr= True)
+            dict(
+                type="RotatedResize",
+                min_size= 1024,
+                max_size= 1024
+            ),
+            dict(
+                type="Pad",
+                size_divisor=32
+            ),
+            dict(
+                type="Normalize",
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_bgr=True
+            )
         ],
-        num_workers= 18,
-        batch_size= 1)
+        num_workers=18,
+        batch_size=1)
 )
 
-max_epoch = 12
+max_epoch=12

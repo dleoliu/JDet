b7f5d7a238fb836b39d9e0dcc6e7724f9e3ef49e
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2022-07-30 18:26:44 +0900

    Update configs/rs22/s2anet

--- configs/rs22/s2anet/s2anet_config_hr.py
+++ configs/rs22/s2anet/s2anet_config_hr.py
@@ -1,74 +1,19 @@
-_base_ = 's2anet_r50_fpn_1x_dota.py'
+_base_ = "s2anet_r50_fpn_1x_dota_rotate_balance_ms_fair.py"
 
 dataset = dict(
-    _cover_ = True,
-    val=dict(
-        type="FAIRDataset",
-        dataset_dir='/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0',
-        transforms=[
-            dict(
-                type="RotatedResize",
-                min_size=1024,
-                max_size=1024
-            ),
-            dict(
-                type = "Pad",
-                size_divisor=32),
-            dict(
-                type = "Normalize",
-                mean =  [123.675, 116.28, 103.53],
-                std = [58.395, 57.12, 57.375],
-                to_bgr=False,)
-        ],
-        batch_size=2,
-        num_workers=18,
-        shuffle=True
-    ),
     train=dict(
-        type="FAIRDataset",
-        dataset_dir='/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0',
-        transforms=[
-            dict(
-                type="RotatedResize",
-                min_size=1024,
-                max_size=1024
-            ),
-            dict(
-                type='RotatedRandomFlip', 
-                prob=0.5),
-            dict(
-                type = "Pad",
-                size_divisor=32),
-            dict(
-                type = "Normalize",
-                mean =  [123.675, 116.28, 103.53],
-                std = [58.395, 57.12, 57.375],
-                to_bgr=False,)
-        ],
-        batch_size=2, 
+        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0",
+        batch_size=4,
         num_workers=18,
-        shuffle=True
     ),
-    test = dict(
-        type= "ImageDataset",
-        dataset_type="FAIR",
-        images_dir= "/scratch/acd13834xw/FAIR1M2/processed_1024_hr/test_1024_200_1.0/images/",
-        transforms= [
-        dict(
-            type= "RotatedResize",
-            min_size= 1024,
-            max_size= 1024),
-        dict(
-                type = "Pad",
-                size_divisor=32),
-        dict(
-            type= "Normalize",
-            mean=  [123.675, 116.28, 103.53],
-            std= [58.395, 57.12, 57.375],
-            to_bgr= False)
-        ],
-        num_workers= 18,
-        batch_size= 1)
+    val=dict(
+        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0",
+        num_workers=18,
+    ),
+    test=dict(
+        images_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/test_1024_200_1.0/images/",
+        num_workers=18,
+    ),
 )
 
 max_epoch = 6

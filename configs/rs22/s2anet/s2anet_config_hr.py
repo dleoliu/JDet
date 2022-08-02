2e8fab017c8ed80d2fda7f41964164d3006186d2
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2022-08-03 00:49:01 +0900

    Update configs/rs22/s2anet

--- configs/rs22/s2anet/s2anet_config_hr.py
+++ configs/rs22/s2anet/s2anet_config_hr.py
@@ -3,17 +3,23 @@ _base_ = "s2anet_r50_fpn_1x_dota_rotate_balance_ms_fair.py"
 dataset = dict(
     train=dict(
         dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0",
-        batch_size=4,
+        batch_size=8,
         num_workers=18,
     ),
     val=dict(
         dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0",
+        batch_size=8,
         num_workers=18,
     ),
     test=dict(
         images_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/test_1024_200_1.0/images/",
+        batch_size=8,
         num_workers=18,
     ),
 )
 
-max_epoch = 6
+optimizer = dict(
+    lr=0.01,
+)
+
+max_epoch = 12

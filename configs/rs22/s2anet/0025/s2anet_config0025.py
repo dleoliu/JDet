071e92948aa458e64153416182a39e7fdd91124b
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2022-08-04 00:45:50 +0900

    Create configs/rs22/s2anet/0025

---
+++ configs/rs22/s2anet/0025/s2anet_config0025.py
@@ -0,0 +1,25 @@
+_base_ = "s2anet_r50_fpn_1x_dota_rotate_balance_ms_fair.py"
+
+dataset = dict(
+    train=dict(
+        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/trainval_1024_200_1.0",
+        batch_size=8,
+        num_workers=18,
+    ),
+    val=dict(
+        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/trainval_1024_200_1.0",
+        batch_size=8,
+        num_workers=18,
+    ),
+    test=dict(
+        images_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/test_1024_200_1.0/images/",
+        batch_size=8,
+        num_workers=18,
+    ),
+)
+
+optimizer = dict(
+    lr=0.0025,
+)
+
+max_epoch = 12

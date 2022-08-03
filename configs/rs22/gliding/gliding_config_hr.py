9a4ffcea148cef3839244578a496086119ac3dee
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2022-08-04 00:23:15 +0900

    Update configs/rs22/gliding

--- configs/rs22/gliding/gliding_config_hr.py
+++ configs/rs22/gliding/gliding_config_hr.py
@@ -9,19 +9,19 @@ model = dict(
 dataset = dict(
     train=dict(
         type="FAIRDataset",
-        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/trainval_1024_200_1.0",
+        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0",
         batch_size=8,
         num_workers=18,
     ),
     val=dict(
         type="FAIRDataset",
-        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/trainval_1024_200_1.0",
+        dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/trainval_1024_200_1.0",
         batch_size=8,
         num_workers=18,
     ),
     test=dict(
         dataset_type="FAIR",
-        images_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/test_1024_200_1.0/images/",
+        images_dir="/scratch/acd13834xw/FAIR1M2/processed_1024_hr/test_1024_200_1.0/images/",
         batch_size=8,
         num_workers=18,
     ),

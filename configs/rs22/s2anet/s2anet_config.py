9560986f5a479051b9628d93ec41df3cc4fb4a36
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2023-03-17 16:12:05 +0900

    Update configs/rs22/s2anet

--- configs/rs22/s2anet/s2anet_config.py
+++ configs/rs22/s2anet/s2anet_config.py
@@ -1,5 +1,13 @@
 _base_ = "s2anet_r50_fpn_1x_dota_rotate_balance_ms_fair.py"
 
+model = dict(
+    bbox_head=dict(
+        test_cfg=dict(
+            score_thr=0.5,
+        )
+    )
+)
+
 dataset = dict(
     train=dict(
         dataset_dir="/scratch/acd13834xw/FAIR1M2/processed_1024/trainval_1024_200_1.0",
